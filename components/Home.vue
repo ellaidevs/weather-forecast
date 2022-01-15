8d4a1fec26a9ef156416285d731c0c14d44a7b73
ellai <ellaidevs@gmail.com>
2022-01-15 23:51:01 +0800

    feat: route to city informations

--- components/Home.vue
+++ components/Home.vue
@@ -15,26 +15,29 @@
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 cursor-pointer"
       >
         <div
+          @click="getCityWeather(city)"
           v-for="city in cities"
           v-bind:key="city"
           class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
         >
-          <img
-            :src="randomImgUrl + city"
-            :alt="city"
-            class="rounded-t-lg w-full"
-          />
-          <div class="p-6">
-            <h1
-              class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200 font-bold text-gray-900"
-            >
-              {{ city }}
-            </h1>
-            <p class="text-gray-700 my-2 hover-text-900">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Praesentium quis.
-            </p>
-          </div>
+          <nuxt-link to="/cities">
+            <img
+              :src="randomImgUrl + city"
+              :alt="city"
+              class="rounded-t-lg w-full"
+            />
+            <div class="p-6">
+              <h1
+                class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200 font-bold text-gray-900"
+              >
+                {{ city }}
+              </h1>
+              <p class="text-gray-700 my-2 hover-text-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium quis.
+              </p>
+            </div>
+          </nuxt-link>
         </div>
       </div>
     </div>
@@ -48,12 +51,10 @@ export default {
     return {
       cities: [
         "Kuala Lumpur",
-        "George Town",
         "Johor Bahru",
         "Kuching",
         "Kota Kinabalu",
         "Ipoh",
-        "Johor Bahru",
         "George Town",
         "Selangor",
         "Kuantan",
@@ -78,6 +79,9 @@ export default {
         "api.openweathermap.org/data/2.5/weather?q=brisbane&appid=a200ce27073c1f74acfcc6246a889d93"
       );
       console.log("check resp:", response);
+    },
+    getCityWeather(city) {
+      console.log("check wether", city);
     },
   },
 };
