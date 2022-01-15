f19606f6c26a5ad6c4d19989fa23844bcbd574c5
ellai <ellaidevs@gmail.com>
2022-01-16 00:10:08 +0800

    chore: make component for cities grid

---
+++ components/home/CitiesGrid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 cursor-pointer"
+  >
+    <div
+      @click="getCityWeather(city)"
+      v-for="city in cities"
+      v-bind:key="city"
+      class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
+    >
+      <nuxt-link to="/cities">
+        <img
+          :src="randomImgUrl + city"
+          :alt="city"
+          class="rounded-t-lg w-full"
+        />
+        <div class="p-6">
+          <h1
+            class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200 font-bold text-gray-900"
+          >
+            {{ city }}
+          </h1>
+          <p class="text-gray-700 my-2 hover-text-900">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            quis.
+          </p>
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitiesComponent",
+  props: ["cities"],
+  data() {
+    return {
+      randomImgUrl: "https://source.unsplash.com/random/",
+    };
+  },
+};
+</script>
+
+<style></style>
--- components/home/Home.vue
+++ components/home/Home.vue
@@ -3,45 +3,19 @@
   <div>
     <div class="bg-gray-100 min-h-screen py-32 px-10">
       <HomeTitle title="Weather Forecast" />
-      <div
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 cursor-pointer"
-      >
-        <div
-          @click="getCityWeather(city)"
-          v-for="city in cities"
-          v-bind:key="city"
-          class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
-        >
-          <nuxt-link to="/cities">
-            <img
-              :src="randomImgUrl + city"
-              :alt="city"
-              class="rounded-t-lg w-full"
-            />
-            <div class="p-6">
-              <h1
-                class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200 font-bold text-gray-900"
-              >
-                {{ city }}
-              </h1>
-              <p class="text-gray-700 my-2 hover-text-900">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Praesentium quis.
-              </p>
-            </div>
-          </nuxt-link>
-        </div>
-      </div>
+      <CitiesGrid :cities="cities" />
     </div>
   </div>
 </template>
 
 <script>
 import HomeTitle from "./HomeTitle.vue";
+import CitiesGrid from "./CitiesGrid.vue";
 export default {
   name: "NuxtTutorial",
   components: {
     HomeTitle,
+    CitiesGrid,
   },
   data() {
     return {
@@ -66,7 +40,6 @@ export default {
         "Putrajaya",
         "Melaka",
       ],
-      randomImgUrl: "https://source.unsplash.com/random/",
     };
   },
   methods: {
